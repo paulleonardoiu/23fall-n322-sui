98472a6e15183ce588b315ba5d85743ca562a3dd
paulleonardo <pauleon@iu.edu>
2023-11-18 23:26:36 -0500

    Initial Commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,30 @@
-import '@/styles/globals.css'
+import React from 'react';
+import 'semantic-ui-css/semantic.css';
+import '@/styles/globals.css';
+import { Menu, Input, Button } from 'semantic-ui-react';
+import Link from 'next/link';
+import styles from '@/styles/Home.module.css';
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [pokemonSearch, setPokemonSearch] = React.useState('');
+
+  function updateSearch(e, {value}) {
+    setPokemonSearch(value);
+  }
+  return (
+    <>
+
+      <div className={styles.siteContainer}>
+        <Menu>
+          <Menu.Item as={Link} href='/'>Home</Menu.Item>
+          <Menu.Item>
+            <Input name='pokemonSearch' onChange={updateSearch} value={pokemonSearch}/>
+            <Button as={Link} href={`/pokemon/${pokemonSearch}`}>Search</Button>
+          </Menu.Item>
+        </Menu>
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
---
+++ src/pages/pokemon/[name].js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { Card, Loader, Image } from 'semantic-ui-react';
+import styles from '@/styles/Pokemon.module.css';
+
+export default function PokemonNamePage() {
+    const [pokemonInfo, setPokemonInfo] = React.useState({ loading: true })
+    const router = useRouter();
+
+    React.useEffect(function(){
+        if(pokemonInfo.name !== router.query.name && router.query.name){
+            console.log('Load in pokemon info');
+            fetch(`https://pokeapi.co/api/v2/pokemon/${router.query.name}`)
+            .then(r=> r.json())
+            .then(function (r){
+                setPokemonInfo(r);
+            })
+            .catch((e)=>setPokemonInfo({loading: false, name: router.query.name}));
+        }
+    })
+
+    // console.log(router.query);
+
+    return (
+        <>
+            <div className={styles.pokemon}>
+                <Card>
+                    <h1>Pokemon Name: {router.query.name}</h1>
+                    <Loader active={pokemonInfo.loading || pokemonInfo.name !== router.query.name}/>
+                    {
+                        // if this statement
+                        pokemonInfo.id ? (
+                        // then do this; true
+                         <>
+                         <Image src={pokemonInfo.sprites.front_default}></Image>
+                            <h2>Type: {pokemonInfo.types[0].type.name}</h2>
+                            <h2>Move: {pokemonInfo.moves[0].move.name}</h2>                        
+                            <h2>Weight: {pokemonInfo.weight} lbs</h2>
+                        </>
+                        ) : ( 
+                        // else do this; false
+                        <>
+                            {isNaN(pokemonInfo.id)
+                            ? <h2>Searching for Pokemon</h2>
+                        : <h2>Pokemon Not Found</h2>
+                        }
+                        </>
+                        )
+                    }
+                </Card>
+            </div>
+        </>
+    )
+}
